f3bb06d1dba09113c8f5fd9b740b3380e34d392c
LP5EBischoff <elio.bischoff@gmail.com>
2026-02-24 07:59:00 +0100

    Updated

--- src/main/java/ch/bbw/eb/helloworld/HelloWorldApplication.java
+++ src/main/java/ch/bbw/eb/helloworld/HelloWorldApplication.java
@@ -1,10 +1,13 @@
 package ch.bbw.eb.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
--- src/main/java/ch/bbw/eb/helloworld/ThisController.java
+++ src/main/java/ch/bbw/eb/helloworld/ThisController.java
@@ -1,14 +1,19 @@
 package ch.bbw.eb.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThisController {
+    private static final Logger log = LoggerFactory.getLogger(ThisController.class);
 
     @GetMapping("/helloWorld")
-    public ResponseEntity<String> helloWorld(){
+    public ResponseEntity<String> helloWorld() {
+        log.info("Hello World Application");
+
         return ResponseEntity.ok("Hello World");
     }
 }
